9bc97a4549a210693fd843239f5787be90c41f25
leionz <2984353371@qq.com>
2025-03-10 17:23:42 +0800

    fix:  modify v-loading filename and style

--- docs/.vitepress/components/vLoading/LoadingDemo.vue
+++ docs/.vitepress/components/vLoading/LoadingDemo.vue
@@ -1,15 +1,35 @@
 <template>
-  <div>
+  <div class="banner">
     <button @click="loading = !loading">切换loading</button>
-    <div v-loading="loading"></div>
+    <div class="main" v-loading="loading"></div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import { vLoading } from '@cp-vuedir/core';
 const loading = ref(false)
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="css" scoped>
+.banner{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+button{
+    height: 30px;
+    background-color: #69d0dd;
+    margin-bottom: 10px;
+    border-radius: 5px;
+  }
+  .main{
+    width: 200px;
+    height: 200px;
+    background: #eeb6b6;
+    border-radius: 5px;
+  }
 </style>
